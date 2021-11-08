735111d7f85bf840155e9dfb28dca88a8cc4ce6c
tomcoolpxl <71908551+tomcoolpxl@users.noreply.github.com>
2021-11-08 16:29:37 +0100

    switched from kotlin to java :)

---
+++ simplewebservice/src/test/java/be/pxl/simplewebservice/SimpleWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package be.pxl.simplewebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
