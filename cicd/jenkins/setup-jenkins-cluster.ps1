312b2c593c562e8a363955235fda48c9c80323b3
tomcoolpxl <71908551+tomcoolpxl@users.noreply.github.com>
2021-11-10 02:41:38 +0100

    fixes

--- cicd/jenkins/setup-jenkins-cluster.ps1
+++ cicd/jenkins/setup-jenkins-cluster.ps1
@@ -43,51 +43,12 @@ kubectl get services --namespace devops-tools
 kubectl get pods --namespace devops-tools -o wide
 kubectl logs --namespace devops-tools <jenkins-pod-name>
 # kubectl exec -it <jenkins-pod-name> cat  /var/jenkins_home/secrets/initialAdminPassword -n devops-tools
-# kubectl port-forward service/jenkins 6420:8080 --namespac jenkins
+# alternatief kubectl port-forward service/jenkins 6420:8080 --namespace jenkins
 # browse naar localhost:8000
-# https://digitalavenue.dev/How-To-Setup-Jenkins-On-Kubernetes/
-# Until Kubernetes 1.4 removes the SNATing of source ips, seems that CSRF (enabled by default in Jenkins 2) needs to be configured to avoid WARNING: No valid crumb was included in request errors. This can be done checking Enable proxy compatibility under Manage Jenkins -> Configure Global Security
+
 $dockerserver = 'https://index.docker.io/v1/'
 $dockerusername = 'tomcoolpxl'
 $dockerpassword = 'password'
 $dockeremail = 'tom.cool@pxl.be'
-
-# kubectl create secret docker-registry regcred --docker-server=$dockerserver --docker-username=$dockerusername --docker-password=$dockerpassword --docker-email=$dockeremail
-kubectl create secret docker-registry regcred --docker-server=$dockerserver --docker-username=$dockerusername --docker-password=$dockerpassword --docker-email=$dockeremail --namespace devops-tools
-# https://devopscube.com/build-docker-image-kubernetes-pod/
-# https://devopscube.com/setup-jenkins-on-kubernetes-cluster
-# https://devopscube.com/jenkins-build-agents-kubernetes/
-# https://itnext.io/jenkins-running-workers-in-kubernetes-and-docker-images-build-83299a10f3ca
-
-
-install kubernetes, mavin plugins, kubernetes cli plugin
-turn on proxy compatibility
-
-kubernetes:
-namespace: devops-tools
-jenkins URL:
-http://jenkins-service.devops-tools.svc.cluster.local:8080
-pod label:
-jenkins
-agent
-pod template:
-name: kube-agent
-namespace: devops-tools
-labels: kubeagent
-add container template
-
-
-new pipeline
-git -> https://github.com/systemsexpert-pxl/selabs.git
-create credentials voor github
-branch specifier empty
-invoke top-level Maven targets -> 'clean package'
-
-Tools config
-jdk-11
-https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.13%2B8/OpenJDK11U-jdk_x64_linux_hotspot_11.0.13_8.tar.gz
-jdk-11.0.13+8
-
-env var:
-JAVA_HOME
-/var/jenkins_home/tools/hudson.model.JDK/jdk-11/jdk-11.0.13+8
+# kubectl create secret docker-registry dockercred --docker-server=$dockerserver --docker-username=$dockerusername --docker-password=$dockerpassword --docker-email=$dockeremail
+kubectl create secret docker-registry dockercred --docker-server=$dockerserver --docker-username=$dockerusername --docker-password=$dockerpassword --docker-email=$dockeremail --namespace devops-tools
