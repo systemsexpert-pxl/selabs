5572e94f4550b9e695d6761adca04319c456d57d
Tom Cool <71908551+tomcoolpxl@users.noreply.github.com>
2021-12-01 11:32:26 +0100

    Update setup-cluster.ps1

--- helm/setup-cluster.ps1
+++ helm/setup-cluster.ps1
@@ -1,6 +1,6 @@
 # create cluster with k3d config file
 k3d mycluster create --config .\k3d-config.yaml
-$clustername = "my-cluster"
+$clustername = "mycluster"
 # $clustername = $(cat .\k3d-config.yaml | Select-String -Pattern 'name: ').Line.Remove(0, 6)
 # extract kubeconfig
 $KUBECONFIG = "$HOME\k3d\kubeconfig"
